fb85b4217bbb64be9269a3dcd9cf2f291b2a8341
fleuryloic <fleury.loic@hotmail.fr>
2025-05-28 20:51:08 +0200

    Update map_database.cc

--- src/openvslam/data/map_database.cc
+++ src/openvslam/data/map_database.cc
@@ -400,13 +400,16 @@ void map_database::to_json(nlohmann::json& json_keyfrms, nlohmann::json& json_la
         spdlog::info("exist? {}", keyfrms.count(std::to_string(id)));
         assert(!keyfrms.count(std::to_string(id)));
         keyfrms[std::to_string(id)] = keyfrm->to_json();
+        spdlog::info("finished frame");
     }
+    spdlog::info("encoding {} keyframes to store", keyframes_.size());
+    spdlog::info("done all frames");
     json_keyfrms = keyfrms;
 
     // Save each 3D point as json
-    spdlog::info("encoding {} landmarks to store", landmarks_.size());
+    //spdlog::info("encoding {} landmarks to store", landmarks_.size());
     std::map<std::string, nlohmann::json> landmarks;
-    for (const auto id_lm : landmarks_) {
+    /*for (const auto id_lm : landmarks_) {
         const auto id = id_lm.first;
         const auto& lm = id_lm.second;
         assert(lm);
@@ -416,6 +419,7 @@ void map_database::to_json(nlohmann::json& json_keyfrms, nlohmann::json& json_la
         assert(!landmarks.count(std::to_string(id)));
         landmarks[std::to_string(id)] = lm->to_json();
     }
+    */
     json_landmarks = landmarks;
 }
 
